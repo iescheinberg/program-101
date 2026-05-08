52deeef6ce45ae70f9c4aa7e64b34eef3a23889e
Ezequiel Scheinberg <ezequiel.scheinberg@devlights.com>
2026-05-08 17:18:27 -0300

    first commit

---
+++ section-2.ts
@@ -0,0 +1,16 @@
+let numberOfLines  = 1;
+
+export function printLineNumber() {
+    numberOfLines++;
+    console.log('Linea #', numberOfLines);
+}
+
+console.log('Linea #', numberOfLines);
+
+printLineNumber();
+
+printLineNumber();
+
+printLineNumber();
+
+
