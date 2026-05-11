519feda0c9ad40555cf74ccff64572f0dee4abd1
Ezequiel Scheinberg <ezequiel.scheinberg@devlights.com>
2026-05-11 17:03:07 -0300

    add if-else section and homework

--- app.ts
+++ app.ts
@@ -1,3 +1,23 @@
 
+/* 
+A >= 90
+B >= 80
+C >= 70
+D >= 60
+F < 60
+*/
 
-console.log('Hello, world!!!');
+let grade:number = 65
+
+
+if (grade >= 90) {
+    console.log('Aprobado con: A')
+} else if (grade >= 80) {
+    console.log('Aprobado con: B')
+} else if (grade >= 70) {
+    console.log('Aprobado con: C')
+} else if (grade >= 60) {
+    console.log('Aprobado con: D')
+} else {
+    console.log('Reprobado con: F')
+}
---
+++ section/if-else.ts
@@ -0,0 +1,23 @@
+
+
+console.log('Inicio del programa');
+
+// let isTired = true
+
+// if (isTired) {
+//     console.log('Tomar café')
+// } else {
+//     console.log('Tomar agua')
+// }
+
+export let grade:number = 55
+
+if (grade >= 60) {
+    console.log('El alumno aprobo la clase!')
+} else if (grade > 50) {
+    console.log('Por favor, estudie mas!')
+} else {
+    console.log('El alumno reprobo la clase!')
+}
+
+console.log('Fin del programa');
